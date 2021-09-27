d975f0fb8a61c4c484302d5faac32bf655040895
Erwan Egasse <erwan.egasse@gmail.com>
2021-09-27 11:09:21 +0200

    Add get_posts API endpoint

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,19 +2,25 @@
 
 namespace App\Controller;
 
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class PostController extends AbstractController
 {
     /**
-     * @Route("/post", name="post")
+     * @Route("/api/post", name="api_post_get_posts", methods="GET")
      */
-    public function index(): Response
+    public function get_posts(PostRepository $post_repo, SerializerInterface $serializer): Response
     {
-        return $this->render('post/index.html.twig', [
-            'controller_name' => 'PostController',
-        ]);
+        // Dependency injection, Symfony will automatically handle the routes, parameters and requests
+        $all_posts = $post_repo->findAll();
+
+        // Queried Posts need to be normalized before being json_encoded
+        $posts_json = $serializer->serialize($all_posts, 'json');
+
+        return new Response($posts_json, Response::HTTP_OK, ['Content-Type' => 'application/json']);
     }
 }
