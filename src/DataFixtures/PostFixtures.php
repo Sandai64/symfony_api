ba9ad12e8b0c4b49ab56650a7985baa8b758a5f2
Erwan Egasse <erwan.egasse@gmail.com>
2021-09-27 09:44:29 +0200

    Fill fixture

--- src/DataFixtures/PostFixtures.php
+++ src/DataFixtures/PostFixtures.php
@@ -2,15 +2,29 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Post;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class PostFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // Initialize Faker
+        $faker = Factory::create('fr_FR');
+
+        for ($i = 0; $i <= 6; $i++)
+        {
+            $post = new Post();
+            
+            // We can chain object setters like so
+            $post->setTitle($faker->sentence())
+                 ->setContent($faker->paragraphs(8), true)
+                 ->setCreatedAt($faker->dateTimeBetween('-6 months'));
+
+            $manager->persist($post);
+        }
 
         $manager->flush();
     }
